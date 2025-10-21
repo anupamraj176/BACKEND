faa4e8e7f34bde5f94d6920a53f62d1ca1c40227
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-10-21 14:42:02 +0530

    DAY-06

--- AUTH/index.js
+++ AUTH/index.js
@@ -5,9 +5,14 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+//import cookie parser
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.use(express.json());
 
 require('./config/database').connectDB();
+
 
 //route import kro or mount kro
 const user = require('./route/user')
